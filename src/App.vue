39684f764dc8c8291218070932cdb1597d9c3d73
chendefeng <1257340960@qq.com>
2025-08-28 16:56:15 +0800

    feat:Ant Design Vue 默认中文

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <div id="app">
-    <BasicLayout />
+    <a-config-provider :locale="zhCN">
+      <BasicLayout />
+    </a-config-provider>
   </div>
 
-  <!--  <RouterView />-->
 </template>
 <script setup lang="ts">
+import zhCN from 'ant-design-vue/es/locale/zh_CN'
+import dayjs from 'dayjs'
+import 'dayjs/locale/zh-cn'
+dayjs().locale('zh-cn')
 import BasicLayout from '@/layouts/BasicLayout.vue'
 </script>
 
